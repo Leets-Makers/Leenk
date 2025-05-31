92de8c91ea9a8388535bfb7929ad5ddc5d43ed3a
huncozyboy <leejh5838@naver.com>
2025-05-31 14:58:47 +0900

    feat : 링크 참여자 엔티티(중간 테이블) 추가

---
+++ src/main/java/leets/leenk/domain/leenk/domain/entity/LeenkParticipants.java
@@ -0,0 +1,48 @@
+package leets.leenk.domain.leenk.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@Table(name = "leenk_participants")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LeenkParticipants {
+
+    @Id
+    @Column(name = "leenk_participant_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "leenk_id", nullable = false, updatable = false)
+    private Leenks leenk;
+
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+//    private User user;
+
+    @Column(name = "joined_at", nullable = false)
+    private LocalDateTime joinedAt;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Builder.Default
+    @Column(name = "is_host", nullable = false)
+    private Boolean isHost = false;
+
+    @Column(name = "current_participants", nullable = false)
+    private Long currentParticipants;
+}

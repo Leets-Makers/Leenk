30057cf40111d5ca2c46513da55b1cbaba7a7622
hyxklee <ewgt1234@naver.com>
2025-06-03 21:45:55 +0900

    feat: 인증 후 유저 매핑 메서드 구현

--- src/main/java/leets/leenk/domain/user/application/mapper/UserMapper.java
+++ src/main/java/leets/leenk/domain/user/application/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package leets.leenk.domain.user.application.mapper;
 
 import leets.leenk.domain.user.application.dto.response.UserInfoResponse;
 import leets.leenk.domain.user.domain.entity.User;
+import leets.leenk.global.auth.application.dto.response.OauthUserInfoResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,6 +19,14 @@ public class UserMapper {
                 .introduction(user.getIntroduction())
                 .mbti(user.getMbti())
                 .totalReactionCount(user.getTotalReactionCount())
+                .build();
+    }
+
+    public User toUser(OauthUserInfoResponse oauthUserInfoResponse) {
+        return User.builder()
+                .id(oauthUserInfoResponse.userId())
+                .name(oauthUserInfoResponse.name())
+                .cardinal(oauthUserInfoResponse.cardinal())
                 .build();
     }
 }
--- src/main/java/leets/leenk/global/auth/application/dto/response/OauthUserInfoResponse.java
+++ src/main/java/leets/leenk/global/auth/application/dto/response/OauthUserInfoResponse.java
@@ -3,8 +3,6 @@ package leets.leenk.global.auth.application.dto.response;
 public record OauthUserInfoResponse(
         long userId,
         String name,
-        String email,
-        String tel,
         int cardinal
 ) {
 }

05bf2dbd113464274d031020cbf25ef1fc3bb44a
jj <syeonie0526@gmail.com>
2025-06-05 02:14:07 +0900

    feat: SQS 이벤트 핸들러 구현

---
+++ src/main/java/leets/leenk/domain/notification/application/service/AwsSqsManager.java
@@ -0,0 +1,47 @@
+package leets.leenk.domain.notification.application.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import leets.leenk.domain.notification.config.AwsSqsProperties;
+import leets.leenk.domain.notification.application.dto.SqsMessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class AwsSqsManager {
+
+	private final AwsSqsProperties awsSqsProperties;
+	private final SqsAsyncClient sqsAsyncClient;
+
+	public void sendMessageToQueue(SendMessageRequest event) {
+		sqsAsyncClient.sendMessage(event);
+	}
+
+	public SendMessageRequest createPushAlarmMessageRequest(SqsMessageEvent event) {
+		return SendMessageRequest.builder()
+			.queueUrl(awsSqsProperties.getQueueUrl())
+			.delaySeconds(awsSqsProperties.getMessageDelaySecs())
+			.messageBody(event.getContent())
+			.messageAttributes(Map.of(
+				"title", convertToAttributeValue(event.getTitle()),
+				"deviceToken", convertToAttributeValue(event.getDeviceToken())
+			))
+			.build();
+	}
+
+
+	private MessageAttributeValue convertToAttributeValue(String value) {
+		log.debug("convertToAttributeValue = {}", value);
+		return MessageAttributeValue.builder()
+			.dataType("String")
+			.stringValue(value)
+			.build();
+	}
+}
---
+++ src/main/java/leets/leenk/domain/notification/handler/SqsEventHandler.java
@@ -0,0 +1,26 @@
+package leets.leenk.domain.notification.handler;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import leets.leenk.domain.notification.application.service.AwsSqsManager;
+import leets.leenk.domain.notification.application.dto.SqsMessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class SqsEventHandler {
+
+	private final AwsSqsManager awsSqsManager;
+
+	@EventListener
+	public void handleEvent(SqsMessageEvent event) {
+		log.info("handleEvent event = {}", event);
+
+		SendMessageRequest request = awsSqsManager.createPushAlarmMessageRequest(event);
+		awsSqsManager.sendMessageToQueue(request);
+	}
+}

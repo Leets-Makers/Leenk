271294c4e816541174080a5238a50f5f05876190
hyxklee <ewgt1234@naver.com>
2025-06-09 16:03:41 +0900

    feat: 인증 객체에서 userId 반환하는 커스텀 어노테이션 구현

---
+++ src/main/java/leets/leenk/global/auth/application/annotation/CurrentUserResolver.java
@@ -0,0 +1,38 @@
+package leets.leenk.global.auth.application.annotation;
+
+import leets.leenk.global.auth.application.exception.OauthException;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static leets.leenk.global.auth.application.usecase.AuthUsecase.JWT_USER_ID_CLAIM;
+
+@Component
+public class CurrentUserResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUserId.class)
+                && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof Jwt jwt)) {
+            throw new OauthException();
+        }
+
+        return Long.valueOf(jwt.getClaimAsString(JWT_USER_ID_CLAIM));
+    }
+}

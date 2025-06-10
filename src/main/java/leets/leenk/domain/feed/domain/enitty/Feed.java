19de9b95768a292b46286ba4f521a64448b66be9
hyxklee <ewgt1234@naver.com>
2025-06-10 14:25:07 +0900

    feat: feed 관련 엔티티 기본 설정

---
+++ src/main/java/leets/leenk/domain/feed/domain/enitty/Feed.java
@@ -0,0 +1,36 @@
+package leets.leenk.domain.feed.domain.enitty;
+
+import jakarta.persistence.*;
+import leets.leenk.domain.user.domain.entity.User;
+import leets.leenk.global.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@SuperBuilder
+@Table(name="feeds")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feed extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String content;
+
+    @Column(nullable = false)
+    private long totalReactionCount;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    private LocalDateTime deletedAt;
+
+}

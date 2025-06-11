1e4f4dc3a63aa3de418d512b1a4170c14d0db18c
hyxklee <ewgt1234@naver.com>
2025-06-11 14:55:01 +0900

    refactor: 공통 응답 분리 및 ObjectMapper 주입으로 변경

--- src/main/java/leets/leenk/global/auth/domain/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/leets/leenk/global/auth/domain/handler/CustomAuthenticationEntryPoint.java
@@ -1,31 +1,27 @@
 package leets.leenk.global.auth.domain.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import leets.leenk.global.auth.application.exception.ErrorCode;
-import leets.leenk.global.common.response.CommonResponse;
+import leets.leenk.global.auth.application.util.HandlerResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final HandlerResponseUtil handlerResponseUtil;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        setResponse(response, authException.getMessage());
-    }
-
-    private void setResponse(HttpServletResponse response, String errorMessage) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        String message = new ObjectMapper().writeValueAsString(CommonResponse.error(ErrorCode.OAUTH_ERROR, errorMessage));
-        response.getWriter().write(message);
+        handlerResponseUtil.setResponse(response, HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage(), ErrorCode.OAUTH_ERROR);
     }
 }

42186112a5300b1e9613138499ecb4a3cac28f4f
jj <syeonie0526@gmail.com>
2025-06-14 02:08:56 +0900

    refactor: AwsSqsProperties 클래스에 @ConfigurationProperties 적용 및 Setter 추가

--- src/main/java/leets/leenk/global/sqs/config/AwsSqsProperties.java
+++ src/main/java/leets/leenk/global/sqs/config/AwsSqsProperties.java
@@ -1,18 +1,18 @@
 package leets.leenk.global.sqs.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
+@ConfigurationProperties(prefix = "myapp.sqs")
 @Component
 public class AwsSqsProperties {
 
-	@Value("${myapp.sqs.queue-name}")
 	private String queueName;
-	@Value("${myapp.sqs.queue-url}")
 	private String queueUrl;
-	@Value("${myapp.sqs.messageDelaySecs}")
 	private int messageDelaySecs;
 }

3e8ac01fe19a17f0c7f548441f8b1c627c1f4542
hyxklee <ewgt1234@naver.com>
2025-06-13 12:22:13 +0900

    feat: dto 스웨거 스키마 추가

--- src/main/java/leets/leenk/domain/media/domain/application/dto/request/FeedMediaRequest.java
+++ src/main/java/leets/leenk/domain/media/domain/application/dto/request/FeedMediaRequest.java
@@ -1,18 +1,21 @@
-package leets.leenk.domain.media.domain.application.dto.response;
-
+package leets.leenk.domain.media.domain.application.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import leets.leenk.domain.media.domain.entity.enums.MediaType;
-import lombok.Builder;
 
-@Builder
-public record FeedMediaResponse(
-        @Schema(description = "1부터 시작하는 정수", example = "1")
+public record FeedMediaRequest(
+        @NotNull
+        @Positive
+        @Schema(description = "1부터 시작하는 정수")
         int position,
 
+        @NotNull
         @Schema(description = "미디어 S3 URL", example = "https://s3.example.com/image.jpg")
         String mediaUrl,
 
+        @NotNull
         @Schema(description = "이미지, 비디오", example = "IMAGE or VIDEO")
         MediaType mediaType
 ) {

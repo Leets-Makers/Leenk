6cf25bae97f3d70c3f17d120e6b866e7a6fca7f1
hyxklee <ewgt1234@naver.com>
2025-06-15 16:54:17 +0900

    refactor: 래퍼 타입으로 변경

--- src/main/java/leets/leenk/domain/media/domain/application/dto/request/FeedMediaRequest.java
+++ src/main/java/leets/leenk/domain/media/domain/application/dto/request/FeedMediaRequest.java
@@ -1,6 +1,7 @@
 package leets.leenk.domain.media.domain.application.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import leets.leenk.domain.media.domain.entity.enums.MediaType;
@@ -9,9 +10,9 @@ public record FeedMediaRequest(
         @NotNull
         @Positive
         @Schema(description = "1부터 시작하는 정수")
-        int position,
+        Integer position,
 
-        @NotNull
+        @NotBlank
         @Schema(description = "미디어 S3 URL", example = "https://s3.example.com/image.jpg")
         String mediaUrl,
 

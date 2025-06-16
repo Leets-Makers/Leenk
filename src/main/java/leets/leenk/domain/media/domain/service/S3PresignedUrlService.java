2bca32e4745398ee64bb79515124c345e50db994
hyxklee <ewgt1234@naver.com>
2025-06-16 15:40:53 +0900

    feat: 프리 사인드 URL 발급 API 구현

---
+++ src/main/java/leets/leenk/domain/media/domain/service/S3PresignedUrlService.java
@@ -0,0 +1,50 @@
+package leets.leenk.domain.media.domain.service;
+
+import leets.leenk.domain.media.application.dto.response.MediaUrlResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3PresignedUrlService {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public MediaUrlResponse generateUrl(String fileName) {
+        String key = generateKey(fileName);
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        PutObjectPresignRequest request = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(5))
+                .putObjectRequest(putObjectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedUrlRequest = s3Presigner.presignPutObject(request);
+
+        String putUrl = presignedUrlRequest.url().toString();
+
+        return new MediaUrlResponse(fileName, putUrl);
+    }
+
+    private String generateKey(String fileName) {
+        String key = UUID.randomUUID().toString();
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+        return key + "." + extension;
+    }
+}

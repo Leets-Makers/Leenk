cb6b9c69bc3216dae30b5f0b842c8f4366ef0b9c
jj <syeonie0526@gmail.com>
2025-06-23 02:45:51 +0900

    refactor: JPA 및 Mongo 설정 클래스 분리

--- src/main/java/leets/leenk/LeenkApplication.java
+++ src/main/java/leets/leenk/LeenkApplication.java
@@ -2,22 +2,10 @@ package leets.leenk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableAsync
-@EnableMongoAuditing
-@EnableJpaRepositories(basePackages = {
-	"leets.leenk.domain.feed.domain.repository",
-	"leets.leenk.domain.user.domain.repository",
-	"leets.leenk.domain.media.domain.repository"
-})
-@EnableMongoRepositories(basePackages = "leets.leenk.domain.notification.domain.repository")
 public class LeenkApplication {
 
 	public static void main(String[] args) {

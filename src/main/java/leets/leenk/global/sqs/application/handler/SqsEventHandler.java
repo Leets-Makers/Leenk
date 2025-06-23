ced0bc21c6b5e24f306c5b88c7accf024ca1b9e0
jj <syeonie0526@gmail.com>
2025-06-23 19:42:46 +0900

    refactor: SQS 클라이언트에 재시도 전략 추가 및 메시지 전송 실패 예외 처리 로깅 구현

--- src/main/java/leets/leenk/global/sqs/application/handler/SqsEventHandler.java
+++ src/main/java/leets/leenk/global/sqs/application/handler/SqsEventHandler.java
@@ -7,21 +7,23 @@ import org.springframework.transaction.event.TransactionalEventListener;
 import leets.leenk.global.sqs.application.mapper.AwsSqsManager;
 import leets.leenk.global.sqs.application.dto.SqsMessageEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @Component
 @RequiredArgsConstructor
-@Log4j2
+@Slf4j
 public class SqsEventHandler {
 
 	private final AwsSqsManager awsSqsManager;
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void handleEvent(SqsMessageEvent event) {
-		log.info("handleEvent event = {}", event);
-
-		SendMessageRequest request = awsSqsManager.createPushAlarmMessageRequest(event);
-		awsSqsManager.sendMessageToQueue(request);
+		try {
+			SendMessageRequest request = awsSqsManager.createPushAlarmMessageRequest(event);
+			awsSqsManager.sendMessageToQueue(request);
+		} catch (Exception e){
+			log.error("SQS 메세지 전송 실패 : {}", e.getMessage());
+		}
 	}
 }
--- src/main/java/leets/leenk/global/sqs/config/SqsClientConfig.java
+++ src/main/java/leets/leenk/global/sqs/config/SqsClientConfig.java
@@ -1,24 +1,31 @@
 package leets.leenk.global.sqs.config;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.retries.StandardRetryStrategy;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
-public class AmazonSQSConfig {
+public class SqsClientConfig {
 
 	@Value("${spring.cloud.aws.region.static}")
 	private String region;
 
 	@Bean
 	public SqsAsyncClient sqsAsyncClient() {
+		StandardRetryStrategy retryStrategy = StandardRetryStrategy.builder()
+			.maxAttempts(3)
+			.build();
+
 		return SqsAsyncClient.builder()
 			.credentialsProvider(DefaultCredentialsProvider.create())
 			.region(Region.of(region))
+			.overrideConfiguration(o -> o.retryStrategy(retryStrategy))
 			.build();
 	}
 }

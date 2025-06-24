f435a1f7900c49533055ed0b868cefa7d44d5baa
jj <syeonie0526@gmail.com>
2025-06-24 20:07:08 +0900

    refactor: 임시 컨트롤러/서비스 제거 및 좋아요를 공감으로 용어 수정

--- src/main/java/leets/leenk/domain/notification/presentation/NotificationController.java
+++ src/main/java/leets/leenk/domain/notification/presentation/NotificationController.java
@@ -3,7 +3,6 @@ package leets.leenk.domain.notification.presentation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import leets.leenk.domain.notification.application.dto.NotificationCountResponse;
 import leets.leenk.domain.notification.application.dto.NotificationListResponse;
-import leets.leenk.domain.notification.application.service.NotificationService;
 import leets.leenk.domain.notification.application.usecase.NotificationUsecase;
 import leets.leenk.global.auth.application.annotation.CurrentUserId;
 import leets.leenk.global.common.response.CommonResponse;
@@ -26,37 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class NotificationController {
 
-    private final NotificationService notificationService;
     private final NotificationUsecase notificationUsecase;
-
-    // 알림 생성을 위한 임시 컨트롤러. 추후 삭제 예정
-    @Operation(summary = "태그 알림 발행 API")
-    @PostMapping("/tag")
-    public CommonResponse<Void> TagNotification() {
-        notificationService.temporaryTagNotification();
-        return null;
-    }
-
-    @Operation(summary = "임시 첫 좋아요 알림 발행 API")
-    @PostMapping("/first_reaction")
-    public CommonResponse<Void> FeedFirstReactionNotification() {
-        notificationService.temporaryFeedFirstReactionNotification();
-        return null;
-    }
-
-    @Operation(summary = "임시 새로운 피드 알림 발행 API")
-    @PostMapping("/new_feed")
-    public CommonResponse<Void> NewFeedNotification() {
-        notificationService.temporaryNewFeedNotification();
-        return null;
-    }
-
-    @Operation(summary = "임시 누적 공감 알림 발행 API")
-    @PostMapping("/reaction_count")
-    public CommonResponse<Void> ReactionCountNotification() {
-        notificationService.temporaryReactionCountNotification();
-        return null;
-    }
 
     @Operation(summary = "최근 알림 조회 API [무한스크롤] / 사용자의 최근 알림 목록을 페이지 단위로 조회합니다. pageNumber: 0부터 시작")
     @GetMapping()

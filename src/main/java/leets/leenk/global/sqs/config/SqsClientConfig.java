55a0bf44edb2738fc0dc37d9ce96dc64b5e57286
jj <syeonie0526@gmail.com>
2025-06-24 14:03:07 +0900

    refactor: SqsClientConfig을 StaticCredentialsProvider로 자격 증명 방식 변경

--- src/main/java/leets/leenk/global/sqs/config/SqsClientConfig.java
+++ src/main/java/leets/leenk/global/sqs/config/SqsClientConfig.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.retries.StandardRetryStrategy;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -13,6 +14,8 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 @Configuration
 public class SqsClientConfig {
 
+	@Value("${spring.cloud.aws.credentials.access-key}") String accessKey;
+	@Value("${spring.cloud.aws.credentials.secret-key}") String secretKey;
 	@Value("${spring.cloud.aws.region.static}")
 	private String region;
 
@@ -22,8 +25,10 @@ public class SqsClientConfig {
 			.maxAttempts(3)
 			.build();
 
+		AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
 		return SqsAsyncClient.builder()
-			.credentialsProvider(DefaultCredentialsProvider.create())
+			.credentialsProvider(StaticCredentialsProvider.create(credentials))
 			.region(Region.of(region))
 			.overrideConfiguration(o -> o.retryStrategy(retryStrategy))
 			.build();
